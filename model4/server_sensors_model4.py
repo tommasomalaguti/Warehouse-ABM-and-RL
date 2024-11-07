84455dd1ac6dfb9ee0382f89e46f21623f47ea69
tommasomalaguti <146526928+tommasomalaguti@users.noreply.github.com>
2024-11-07 18:40:53 +0100

    model4

---
+++ model4/server_sensors_model4.py
@@ -0,0 +1,71 @@
+from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.ModularVisualization import ModularServer
+from model4_sensors import WarehouseModel, NormalWall, StorageWall, StorageZone, WaitPoint
+from agent_sensors_model4 import Forklift
+
+def agent_portrayal(agent):
+    portrayal = {}
+    if isinstance(agent, Forklift):
+        portrayal = {
+            "Shape": "circle",
+            "Color": "red" if agent.currently_loaded else "blue",
+            "Filled": "true",
+            "Layer": 2,  # Ensure forklift is on top layer
+            "r": 0.5
+        }
+    elif isinstance(agent, NormalWall):
+        portrayal = {
+            "Shape": "rect",
+            "Color": agent.color,
+            "Filled": "true",
+            "Layer": 1,
+            "w": 1,
+            "h": 1
+        }
+    elif isinstance(agent, StorageWall):
+        portrayal = {
+            "Shape": "rect",
+            "Color": agent.color,
+            "Filled": "true",
+            "Layer": 1,
+            "w": 1,
+            "h": 1
+        }
+    elif isinstance(agent, StorageZone):
+        portrayal = {
+            "Shape": "rect",
+            "Color": "green",
+            "Filled": "true",
+            "Layer": 1,
+            "w": 1,
+            "h": 1
+        }
+    elif isinstance(agent, WaitPoint):
+        portrayal = {
+            "Shape": "rect",
+            "Color": agent.color,
+            "Filled": "true",
+            "Layer": 0,  # Ensure waitpoint is on a lower layer
+            "w": 1,
+            "h": 1
+        }
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 45, 30, 800, 500)
+chart = ChartModule([{"Label": "Energy Consumption",
+                      "Color": "Black"},
+                     {"Label": "Total Distance",
+                      "Color": "Blue"},
+                     {"Label": "Total Time",
+                      "Color": "Red"},
+                     {"Label": "Unloading Cycles",
+                      "Color": "Green"}],
+                    data_collector_name='datacollector')
+
+server = ModularServer(WarehouseModel,
+                       [grid, chart],
+                       "Warehouse Model",
+                       {"width": 30, "height": 30, "max_cycles": 10})
+
+server.port = 8521
+server.launch()
